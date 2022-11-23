8d0865d517b820b93b15d5727e843cc911b6ee23
jipark96 <wlgus996@naver.com>
2022-11-24 00:28:15 +0900

    json

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter  // 자동으로 getter, setter 들어감
+public class HelloData {
+
+    private String username;
+    private int age;
+
+
+}
